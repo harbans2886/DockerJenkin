a6f026b67140962820fddc86ed8f8e40130666e5
harbans2886@gmail.com <mandy@localhost.localdomain>
2021-01-12 12:20:52 +0530

    My First Commit

---
+++ src/main/java/com/mandy/sb/dockerjenkins/SbDockerJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.mandy.sb.dockerjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDockerJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbDockerJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mandy/sb/dockerjenkins/SbDockerJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mandy.sb.dockerjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbDockerJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
