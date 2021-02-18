59c5bed51f825c1c957d75f0e4cff2c33ba59a4f
harbans2886@gmail.com <mandy@localhost.localdomain>
2021-02-18 21:47:13 +0530

    "test Message"

--- src/main/java/com/mandy/sb/dockerjenkins/SbDockerJenkinsApplication.java
+++ src/main/java/com/mandy/sb/dockerjenkins/SbDockerJenkinsApplication.java
@@ -8,6 +8,7 @@ public class SbDockerJenkinsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SbDockerJenkinsApplication.class, args);
+		System.out.println("TEST...");
 	}
 
 }
